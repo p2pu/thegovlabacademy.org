8635d9d1c0788c023d61e45380dc025ce747d4f4
ercchy <erika@serox.si>
2013-10-29 14:10:29 +0100

    working on theme page

--- wp-content/themes/thegovlabacademy/sections/theme_topics.php
+++ wp-content/themes/thegovlabacademy/sections/theme_topics.php
@@ -2,10 +2,10 @@
 $topic_values = simple_fields_fieldgroup('theme_page_topic_group');
 if($topic_values){?>
   <h2>Theme Topics</h2><?php
-  foreach($topic_values as $key=> $value){ ?>
+  foreach($topic_values as $key=> $value){?>
   <div class="theme-topic-list four-col <?php if($key === 0 or $key%4 === 0){echo 'first';} ?>">
     <div class="topic-img">
-      <a class="theme-topic" href="<?php echo get_permalink($value['theme_topics']) ?>">
+      <a class="theme-topic" href="<?php echo $value['theme_topics']['permalink'] ?>">
       <img src="<?php echo $value['theme_page_topic_image']['url'] ?>"
                                 alt="<?php echo $value['theme_page_topic_image'] ?>"/>
       </a>
