4c05d8570800ed59c4866e98bda48c8ff3e2b041
ercchy <erika@serox.si>
2013-10-29 19:24:55 +0100

    styling ul css in post content

--- wp-content/themes/thegovlabacademy/sections/topic_experts.php
+++ wp-content/themes/thegovlabacademy/sections/topic_experts.php
@@ -5,7 +5,7 @@ if ($experts_values){ ?>
   foreach ($experts_values as $key => $values) {
     $name = simple_fields_get_post_value($values, "Image", true);
     $image_url = wp_get_attachment_url(simple_fields_get_post_value($values, "Image", true));?>
-      <li><img src="<?php echo $image_url; ?>" alt="Name of the Expert" title="Name of the Expert"></li>
+      <li><img src="<?php echo $image_url; ?>" alt="Image of <?php echo $name ?>" title="<?php echo $name ?>"></li>
     <?php
   }?>
   </ul><?php
