f6697afc02d4414d91501cc5dc9f88242ff1209a
ercchy <erika@serox.si>
2013-10-29 16:13:44 +0100

    adding get no sidebar page

--- wp-content/themes/thegovlabacademy/no-sidebar.php
+++ wp-content/themes/thegovlabacademy/no-sidebar.php
@@ -3,10 +3,9 @@
  * Template name: No sidebar page
  */
 get_header(); ?>
+ 			<div id="content">
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap clearfix">
+				<div id="inner-content" class="wrapper clearfix">
 
 						<div id="main" class="twelvecol first clearfix" role="main">
 
@@ -14,14 +13,14 @@ get_header(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-								<header class="article-header">
+								<div class="article-header">
 
 									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline vcard"><?php
 										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
 									?></p>
 
-								</header> <?php // end article header ?>
+								</div> <?php // end article header ?>
 
 								<section class="entry-content clearfix">
 									<?php the_content(); ?>
