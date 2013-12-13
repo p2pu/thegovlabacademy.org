eb37c0c5528a1c33151b992aaca5804689c7fe4b
ercchy <erika@serox.si>
2013-12-13 21:56:33 +0100

    digest subscription to govlab

--- wp-content/themes/thegovlabacademy/archive.php
+++ wp-content/themes/thegovlabacademy/archive.php
@@ -1,108 +1,121 @@
 <?php get_header(); ?>
 
-			<div id="content">
+<div id="content">
 
-				<div id="inner-content" class="wrapper clearfix">
+    <div id="inner-content" class="wrapper clearfix">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+        <div id="main" class="eightcol first clearfix" role="main">
 
-							<?php if (is_category()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?>
-								</h1>
+            <?php if (is_category()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e('Posts Categorized:', 'bonestheme'); ?></span> <?php single_cat_title(); ?>
+                </h1>
 
-							<?php } elseif (is_tag()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
-								</h1>
+            <?php } elseif (is_tag()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e('Posts Tagged:', 'bonestheme'); ?></span> <?php single_tag_title(); ?>
+                </h1>
 
-							<?php } elseif (is_author()) {
-								global $post;
-								$author_id = $post->post_author;
-							?>
-								<h1 class="archive-title h2">
+            <?php
+            } elseif (is_author()) {
+                global $post;
+                $author_id = $post->post_author;
+                ?>
+                <h1 class="archive-title h2">
 
-									<span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
+                    <span><?php _e('Posts By:', 'bonestheme'); ?></span> <?php the_author_meta('display_name', $author_id); ?>
 
-								</h1>
-							<?php } elseif (is_day()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?>
-								</h1>
+                </h1>
+            <?php } elseif (is_day()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e('Daily Archives:', 'bonestheme'); ?></span> <?php the_time('l, F j, Y'); ?>
+                </h1>
 
-							<?php } elseif (is_month()) { ?>
-									<h1 class="archive-title h2">
-										<span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?>
-									</h1>
+            <?php } elseif (is_month()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e('Monthly Archives:', 'bonestheme'); ?></span> <?php the_time('F Y'); ?>
+                </h1>
 
-							<?php } elseif (is_year()) { ?>
-									<h1 class="archive-title h2">
-										<span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?>
-									</h1>
-							<?php } ?>
+            <?php } elseif (is_year()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e('Yearly Archives:', 'bonestheme'); ?></span> <?php the_time('Y'); ?>
+                </h1>
+            <?php } ?>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+                <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-								<div class="article-header">
+                    <div class="article-header">
 
-									<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline vcard"><?php
-										printf(__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(__( 'F jS, Y', 'bonestheme' )), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
+                        <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark"
+                                          title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 
-								</div> <?php // end article header ?>
+                        <p class="byline vcard"><?php
+                            printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_category_list(', '));
+                            ?></p>
 
-								<section class="entry-content clearfix">
+                    </div> <?php // end article header ?>
 
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+                    <section class="entry-content clearfix">
 
-									<?php the_excerpt(); ?>
+                        <?php the_post_thumbnail('bones-thumb-300'); ?>
 
-								</section> <?php // end article section ?>
+                        <?php the_excerpt(); ?>
 
-								<footer class="article-footer">
+                    </section> <?php // end article section ?>
 
-								</footer> <?php // end article footer ?>
+                    <footer class="article-footer">
 
-							</article> <?php // end article ?>
+                    </footer> <?php // end article footer ?>
 
-							<?php endwhile; ?>
+                </article> <?php // end article ?>
 
-									<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-										<?php bones_page_navi(); ?>
-									<?php } else { ?>
-										<nav class="wp-prev-next">
-											<ul class="clearfix">
-												<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-												<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-											</ul>
-										</nav>
-									<?php } ?>
+            <?php endwhile; ?>
 
-							<?php else : ?>
+                <?php if (function_exists('bones_page_navi')) { ?>
+                    <?php bones_page_navi(); ?>
+                <?php } else { ?>
+                    <nav class="wp-prev-next">
+                        <ul class="clearfix">
+                            <li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', 'bonestheme')) ?></li>
+                            <li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', 'bonestheme')) ?></li>
+                        </ul>
+                    </nav>
+                <?php } ?>
 
-									<article id="post-not-found" class="hentry clearfix">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+            <?php else : ?>
 
-							<?php endif; ?>
+                <article id="post-not-found" class="hentry clearfix">
+                    <header class="article-header">
+                        <h1><?php _e('Oops, Post Not Found!', 'bonestheme'); ?></h1>
+                    </header>
+                    <section class="entry-content">
+                        <p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'bonestheme'); ?></p>
+                    </section>
+                    <footer class="article-footer">
+                        <p><?php _e('This is the error message in the archive.php template.', 'bonestheme'); ?></p>
+                    </footer>
+                </article>
 
-						</div> <?php // end #main ?>
+            <?php endif; ?>
 
-						<?php get_sidebar(); ?>
+        </div> <?php // end #main ?>
+        <div class="fourcol relative digest-div">
+            <h4 class="widgettitle">Subscribe to GovLab Digest</h4>
+            <form id="digest-form" action="http://eepurl.com/ygd4v" method="post" target="popupwindow"
+                  onsubmit="window.open('http://eepurl.com/ygd4v', 'popupwindow', 'scrollbars=yes,width=650,height=520');return true">
+                <input type="text" name="EMAIL" class="required email" value="E-mail" id="digest-input"
+                       onfocus="if (this.value == 'E-mail') {this.value = '';}"
+                       onblur="if (this.value == '') {this.value = 'E-mail';}">
+                <button type="submit" value="Submit" name="subscribe" id="digest-submit"
+                       class="" >Subscribe</button>
+            </form>
+        </div>
+        <?php get_sidebar(); ?>
 
-								</div> <?php // end #inner-content ?>
+    </div> <?php // end #inner-content ?>
 
-			</div> <?php // end #content ?>
+</div> <?php // end #content ?>
 
 <?php get_footer(); ?>
